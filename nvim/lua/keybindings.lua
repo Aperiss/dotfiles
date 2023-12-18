109ee6627435c8d5bfb8067e1baf9f68d89a0963
Anto <perissanchezantonio@gmail.com>
2023-12-18 13:23:15 +0100

    Initial commit

---
+++ nvim/lua/keybindings.lua
@@ -0,0 +1,24 @@
+local opts = { noremap = true, silent = true}
+local term_opts = { silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
+
+keymap("n", "J", "mzJ`z", opts)
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+keymap("n", "n", "nzzzv", opts)
+keymap("n", "N", "Nzzzv", opts)
+
+keymap("i", "<C-c>", "<Esc>", opts)
+keymap("n", "Q", "<nop>", opts)
+
+keymap("n", "<leader>x", "<cmd> !chmod +x %<CR>", opts)
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,94 @@
+return {
+    -- Which-key
+    {
+        "folke/which-key.nvim",
+        lazy = true,
+    },
+
+    -- Vim-Tmux Navigator
+    {
+        "christoomey/vim-tmux-navigator",
+        lazy = false,
+    },
+
+    -- Autopairs
+    {
+        "windwp/nvim-autopairs"
+    },
+
+    -- Colorscheme
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000
+    },
+
+    ---- Better Indentation
+    ---- Indentation Highlights
+    --{
+    --    "lukas-reineke/indent-blankline.nvim",
+    --},
+    ---- Rainbow Highlights
+    --{
+    --    "HiPhish/nvim-ts-rainbow2",
+    --},
+
+    -- Lualine
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+
+    -- Nvimtree
+    {
+        'nvim-tree/nvim-tree.lua',
+        lazy = true,
+        dependencies = {
+            'nvim-tree/nvim-web-devicons',
+        }
+    },
+
+    -- Telescope (Fuzzy Finder)
+    {
+        'nvim-telescope/telescope.nvim',
+        lazy = true,
+        dependencies = {
+            {'nvim-lua/plenary.nvim'},
+        }
+    },
+
+    -- Treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+    },
+
+    -- Undotree
+    {
+        "jiaoshijie/undotree",
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        }
+    },
+
+    -- Language Support
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    },
+}
