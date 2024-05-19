a60c5f149b4ec3cb6ab32af3726cd51d57ac50b3
Anto <perissanchezantonio@gmail.com>
2024-05-19 18:32:45 +0200

    Adds zsh scripts
    - Adds tmux_launcher scripts

---
+++ .config/zsh/scripts/tmux_launcher.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+tmux_config_path=~/.config/tmux/tmux.conf
+
+# Check if we're inside a tmux sesison
+if [[ -z "$TMUX" ]]; then
+    # List all existing tmux sessions
+    tmux_sessions=($(tmux list-sessions -F "#S" 2>/dev/null))
+
+    case ${#tmux_sessions[@]} in
+        0)
+            tmux -f $tmux_config_path new-session -d -s base
+            tmux attach-session -t base
+            ;;
+        1)
+            tmux attach-session -t ${tmux_sessions[0]}
+            ;;
+        *)
+            echo "Select a tmux session to attach:"
+            i=1
+            for session in "${tmux_sessions[@]}"; do
+                echo "$i: $session"
+                ((i++))
+            done
+            read -r selection
+            if [[ $selection -ge 1 && $selection -le ${#tmux_sessions[@]} ]]; then
+                tmux attach-session -t ${tmux_sessions[$((selection - 1))]}
+            else
+                echo "Invalid selection."
+            fi
+            ;;
+    esac
+fi
