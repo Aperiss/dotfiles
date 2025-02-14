87d5278bd80ad6a516a0b3d81f13322e371047d6
Antonio Peris Sanchez <aperiss@Antonios-MacBook-Pro.local>
2025-02-14 17:50:19 +0100

    Change default alacritty font

--- .config/nvim/lua/core/autocomplete.lua
+++ .config/nvim/lua/core/autocomplete.lua
@@ -33,16 +33,6 @@ return {
                     }),
                 },
 
-                -- sources = {
-                --     {name = 'path'},
-                --     {name = 'copilot'},
-                --     {name = 'nvim_lsp', keyword_length = 2},
-                --     {name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
-                --     {name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
-                --     {name = 'buffer', keyword_length = 2 },        -- source current buffer
-                --     {name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
-                --     {name = 'calc'},                               -- source for math calculation
-                -- },
                 sources = {
                     { name = "path",                    group_index = 2 },
                     { name = "nvim_lsp",                group_index = 1, keyword_length = 2 },
